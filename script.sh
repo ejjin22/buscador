9ad002cfcd70935e72af773f646362e1ae4ce0f1
Ian Ezquerro <ianmasgrande@gmail.com>
2026-09-17 14:27:57 -0600

    Se pide carpeta tambien

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 cadena="$1"
-
+carpeta="$2"
 
 # Valida si se paso una carpeta como argumento
 if [ $# -eq 0 ]; then 
@@ -7,7 +7,9 @@ if [ $# -eq 0 ]; then
         exit 1
 fi
 
-res="$(grep -rH "$cadena" /home)"
+res="$(grep -rH "$cadena" "$carpeta")"
+
+#Checamos si hay alguna coincidencia
 if [ -z "$res" ]; then
 	echo "No se encontro ninguna coincidencia"
 	exit 1
