ec79f5d9c073f90fa320a5f49aafa4da2d99be08
Elizabeth <esjjip222@gmail.com>
2026-09-17 13:50:20 -0600

    add

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 cadena="$1"
 
+
+# Valida si se paso una carpeta como argumento
+if [ $# -eq 0 ]; then 
+	echo "No ingreso ningún argumento"
+        exit 1
+fi
+
 echo "$(grep -rH "$cadena" /home)"
-
-
